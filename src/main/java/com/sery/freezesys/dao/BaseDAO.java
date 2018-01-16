1a25a9528be6a12f268fda5a8ccb0a030f6481c3
251918489@qq.coom <ludan123>
2018-01-16 16:21:57 +0800

    初始化项目

---
+++ src/main/java/com/sery/freezesys/dao/BaseDAO.java
@@ -0,0 +1,10 @@
+package com.sery.freezesys.dao;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * Created by ASUS on 2017/4/9.
+ */
+public interface BaseDAO<T> extends Mapper<T>,MySqlMapper<T> {
+}
